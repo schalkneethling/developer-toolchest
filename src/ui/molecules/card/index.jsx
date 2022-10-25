17ebe0050228c92af138595084902a9498de255a
Geoffrey <info@runningdeveloper.com>
2022-10-25 23:58:08 +0200

    fix: add lazy loading to logo image

--- src/ui/molecules/card/index.jsx
+++ src/ui/molecules/card/index.jsx
@@ -4,7 +4,7 @@ export const Card = ({ result, children }) => {
   const { alt, description, logo, title, url } = result;
   return (
     <li className="card">
-      {logo && <img src={`${process.env.PUBLIC_URL}/logos/${logo}`} height="150" width="250" alt={alt || ""} />}
+      {logo && <img src={`${process.env.PUBLIC_URL}/logos/${logo}`} height="150" width="250" alt={alt || ""} loading="lazy" />}
       <h2>
         <a href={url}>{title}</a>
       </h2>
